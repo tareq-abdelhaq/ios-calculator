aa634d7841e8ce768ce411726e6dffae61738b13
tarek-abdelhak <tareq.abdelhaq98@gmail.com>
2025-10-17 14:49:12 +0300

    calculator v2

--- main.js
+++ main.js
@@ -1,111 +1,98 @@
-const resultScreen = document.querySelector(".result")
-const controlsDiv = document.querySelector(".controls");
+const resultScreenEl = document.querySelector(".result")
+let resultScreen = '0';
 
-let operatorKey = null;
-let operands = [];
+let resultSoFar = 0;
+let prevOperator = null;
 
-let history = [{resultScreen: '0', operandA: null, operandB: null, operatorKey: null}]
-
-const operations =  {
-    'plus': (a,b) => a + b,
-    'minus': (a,b) => a - b,
-    'division': (a,b) => a / b,
-    'multiplication': (a,b) => a * b,
+const operations = {
+    'plus': () => resultSoFar + parseInt(resultScreen),
+    'minus': () => resultSoFar - parseInt(resultScreen),
+    'multiplication': () => resultSoFar * parseInt(resultScreen),
+    'division': () => resultSoFar / parseInt(resultScreen)
 }
 
-function updateHistory (state) {
-    const latestHistoryState = history[history.length - 1];
-    history = [...history, {...latestHistoryState, ...state}]
+function rerenderResultScreen() {
+    resultScreenEl.innerText = resultScreen;
 }
 
-function resetHistory () {
-    history = [{resultScreen: '0', operandA: null, operandB: null, operatorKey: null}]
+function calcOperation() {
+    resultSoFar = operations[prevOperator]()
 }
 
-function updateResultScreen (val) {
-    resultScreen.innerText = val;
-}
 
-function handleClearClick () {
-    updateResultScreen(0)
-    operatorKey = null;
-    operands = [];
-    resetHistory()
-}
-
-function handleOperatorClick(operator) {
-    if (operands.length === 0) {
-        return;
-    }
-    if (!operatorKey) {
-        operatorKey = operator;
-        updateResultScreen('0')
-        updateHistory({operatorKey: operator, resultScreen: '0'})
-    }
-}
-
-function handleNumberClick (number) {
-    if (!operatorKey) {
-        operands[0] = (operands[0] ?? '') + number;
-        updateResultScreen(operands[0])
-        updateHistory({operandA: operands[0], resultScreen: operands[0]})
-    }
-    else {
-        operands[1] = (operands[1] ?? '') + number;
-        updateResultScreen(operands[1])
-        updateHistory({operandB: operands[1], resultScreen: operands[1]})
-    }
-}
-
-function handleEqualClick () {
-    if (operands.length === 0 || !operatorKey) {
-        return;
-    }
-    const operandA = +operands[0]
-    const operandB = +operands[1] ?? 0;
-    const res = operations[operatorKey](operandA,operandB)
-    updateResultScreen(res)
-    updateHistory({resultScreen: res})
-}
-
-function handleBackClick () {
-    if (history.length <= 1) {
-        return;
-    }
-    history.pop();
-
-    const stateToTravel = history[history.length - 1];
-
-    operands[0] = stateToTravel.operandA;
-    operands[1] = stateToTravel.operandB;
-    updateResultScreen(stateToTravel.resultScreen);
-    operatorKey = stateToTravel.operatorKey;
-}
-
-function handleControlsDivClick (e) {
-    if (!e.target instanceof HTMLButtonElement) {
+function handleBtnClick (e) {
+    if (!(e.target instanceof HTMLButtonElement)) {
         return;
     }
     const btnId = e.target.id;
-    switch (btnId) {
-        case "clear":
-            handleClearClick()
-            break;
-        case "plus":
-        case "minus":
-        case "division":
-        case "multiplication":
-            handleOperatorClick(btnId)
-            break;
-        case 'equal':
-            handleEqualClick()
-            break
-        case "back":
-            handleBackClick();
-            break
-        default:
-            handleNumberClick(e.target.innerText)
+        switch (btnId) {
+            case "clear":
+                handleClearClick()
+                break;
+            case "plus":
+            case "minus":
+            case "division":
+            case "multiplication":
+                handleOperatorClick(btnId)
+                break;
+            case 'equal':
+                handleEqualClick()
+                break
+            case "back":
+                handleBackClick();
+                break
+            default:
+                handleNumberClick(e.target.innerText)
+    }
+    rerenderResultScreen()
+}
+
+function handleNumberClick(number) {
+    if (resultScreen === '0') {
+        resultScreen = number;
+    }else {
+        resultScreen += number;
     }
 }
 
-controlsDiv.addEventListener("click", handleControlsDivClick)
+function handleClearClick () {
+    resultScreen = '0';
+}
+function handleOperatorClick(operator) {
+    if (resultScreen === '0') {
+        return;
+    }
+
+    if (resultSoFar === 0) {
+        resultSoFar = parseInt(resultScreen);
+    }else {
+        calcOperation()
+    }
+
+
+    prevOperator = operator;
+    resultScreen = '0';
+}
+function handleEqualClick () {
+    if (!prevOperator) {
+        return;
+    }
+    calcOperation()
+    resultScreen = resultSoFar.toString()
+    prevOperator = null;
+    resultSoFar = 0;
+}
+function handleBackClick() {
+    if (resultScreen.length === 1) {
+        resultScreen = '0';
+    }else {
+        resultScreen = resultScreen.substring(0, resultScreen.length - 1)
+    }
+}
+
+function init () {
+    const calcControls = document.querySelector(".controls");
+    calcControls.addEventListener('click', handleBtnClick)
+}
+
+init()
